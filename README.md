4bbe002f67e48ce349b4b6d4f262b7170f522b6b
Daniel Berge <daniel.berge@bekk.no>
2023-03-25 17:46:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Git Commit Message Generator
+
+This script generates commit messages using the OpenAI GPT-3.5-turbo model based on the provided Git diff. It prompts the user to confirm the commit with the generated message.
+
+## Prerequisites
+
+- [jq](https://stedolan.github.io/jq/)
+- [curl](https://curl.se/)
+- An OpenAI API key
+
+## Setup
+
+1. Clone this repository
+2. Install `jq` and `curl` if you haven't already:
+- For Ubuntu/Debian:
+```
+sudo apt-get install jq curl
+```
+- For macOS (using [Homebrew](https://brew.sh/)):
+```
+brew install jq curl
+```
+
+3. Create a `.env` file in the project root directory with your OpenAI API key:
+```
+echo "export OPENAI_API_KEY=your_api_key_here" > .env
+```
---
+++ commit-generator.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Load the API key from the .env file
+source .env
+
+# Escape the diff for JSON
+escaped_diff=$(echo "$diff" | jq -sR)
+
+# Call the OpenAI API to generate a commit message
+api_key=$OPENAI_API_KEY
+
+# Escape the diff for JSON
+escaped_diff=$(echo "$diff" | jq -sR)
+
+prompt_template='Please generate an imperative concise and informative commit message based on the following Git diff. - means that the line was removed, + means that it was added.  If there is no + or - at the start of the line, you should only use it as context, it has not been changed. The commit message should not end with a period:\n\n'
+instruction='\n\nPlease provide a single-line commit message that briefly describes the changes made in this diff.'
+
+# Construct the JSON payload using jq
+payload=$(jq -n --arg prompt_template "$prompt_template" --arg diff "$escaped_diff" --arg instruction "$instruction" '{model: "gpt-3.5-turbo", messages: [{role: "user", content: ($prompt_template + $diff + $instruction)}] }')
+
+response=$(curl -s https://api.openai.com/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $api_key" \
+  -d "$payload")
+
+# Parse the response and extract the generated commit message
+commit_message=$(echo "$response" | jq -r '.choices[0].message.content' | tr -d '\n')
+
+# Check if the commit message is empty or null
+if [ -z "$commit_message" ] || [ "$commit_message" = "null" ]; then
+  echo "Error: Failed to generate commit message."
+  exit 1
+fi
+
+# Print the commit message
+echo "$commit_message"
+
+# Prompt the user to confirm the commit
+read -p "Commit these changes? [y/N] " confirm
+if [ "$confirm" = "y" ]; then
+  git commit -am "$commit_message"
+else
+  echo "Aborted commit."
+fi
